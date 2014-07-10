053b402e90eff0ff80ef739236bc76106db79a01
Vladimir Litvinchik <litvinchik.vladimir@gmail.com>
2014-07-10 10:00:32 +0300

    Signup view

---
+++ public/app/account/signupCtrl.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    var controllerId = 'signupCtrl';
+    angular.module('app').controller(controllerId, ['$scope', signupCtrl]);
+    function signupCtrl($scope) {
+
+    }
+})();
