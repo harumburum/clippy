ab15c3fbab0f3a1b00c124b066628b47383a164e
Vladimir Litvinchik <litvinchik.vladimir@gmail.com>
2014-08-31 23:36:55 +0300

    Fix signup form

--- public/app/account/signupCtrl.js
+++ public/app/account/signupCtrl.js
@@ -2,8 +2,24 @@
     'use strict';
 
     var controllerId = 'signUpCtrl';
-    angular.module('app').controller(controllerId, ['$scope', controllerId]);
+    angular.module('app').controller(controllerId, ['$scope', signUpCtrl]);
     function signUpCtrl($scope) {
+        var vm = $scope;
+        vm.signup = function(){
+            var newUserData = {
+                username: $scope.email,
+                password: $scope.password,
+                firstName: $scope.firstName,
+                lastName: $scope.lastName
+            };
+
+            mvAuth.createUser(newUserData).then(function(){
+                mvNotifier.notify('User account created');
+                $location.path('/');
+            }, function(reason){
+                mvNotifier.error(reason);
+            });
+        }
 
     }
 })();
